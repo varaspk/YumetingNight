14c23c691a2da13c3d0dbb2d9f9feeb65ccd367d
FERNANDO LLANO <m.nanoc@gmail.com>
2017-10-14 00:51:00 +0200

    añadido organicmaterial

---
+++ Yumeteo/OrganicMaterial.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yumeteo
+{
+    // Definicion material organico
+    public abstract class OrganicMaterial : Material
+    {
+    }
+}
